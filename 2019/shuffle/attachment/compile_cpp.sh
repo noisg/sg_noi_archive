5ea779acdb35c125852b5a9cde94cf251c93a944
p <p>
2019-03-28 02:33:54 +0800

    Add 2019 finals

---
+++ 2019/shuffle/attachment/compile_cpp.sh
@@ -0,0 +1,1 @@
+/usr/bin/g++ -DEVAL -std=gnu++11 -O2 -pipe -static -s -o shuffle grader.cpp shuffle.cpp
---
+++ 2019/shuffle/attachment/shuffle.cpp
@@ -0,0 +1,7 @@
+#include "shuffle.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solve(int N, int B, int K, int Q, int ST) {
+	
+}
